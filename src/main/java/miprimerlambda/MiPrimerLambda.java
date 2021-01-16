055a43bdbcf04326ed1378a36501138a394495d4
Carlos Ordonez <42821609+softwarechido@users.noreply.github.com>
2021-01-15 23:29:32 -0600

    Add files via upload

---
+++ src/main/java/miprimerlambda/MiPrimerLambda.java
@@ -0,0 +1,13 @@
+package miprimerlambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public final class MiPrimerLambda {
+	
+	public static String handleRequest(String arg, Context context) {
+		
+		return arg;
+		
+		
+	}
+}
