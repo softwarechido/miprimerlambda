0810b37a461221770bfbf217e8ff7c155dec0a0f
Carlos Ordonez <42821609+softwarechido@users.noreply.github.com>
2022-04-02 12:21:06 -0600

    Update MiPrimerLambda.java

--- src/main/java/miprimerlambda/MiPrimerLambda.java
+++ src/main/java/miprimerlambda/MiPrimerLambda.java
@@ -1,14 +1,9 @@
 package miprimerlambda;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import java.util.Map;
 
 public final class MiPrimerLambda {
-	
-	public static String handleRequest(Map<String,String>, Context context) {
-		
-		return arg;
-		
-		
+	public static String handleRequest(Map<String,String> event, Context context) {
+		return event.toString();
 	}
 }
